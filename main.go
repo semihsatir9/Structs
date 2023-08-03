5131318785bcecd0ce13b9f3c9c754c6e4e91825
Semih Satır <semihsatir9@hotmail.com>
2023-08-03 18:41:21 +0300

    Pointers and Shortcuts

--- main.go
+++ main.go
@@ -39,14 +39,36 @@ func main(){
 	// p1.lastName = "Yeah"
 	// Also possible. Not recommended.
 
+	// Pointers
+	// &abc = The memory adress of abc object
+	// *abc = The object with the memory adress abc
+	// In order to change the value of a spesific data,
+	// you have to save the pointer and make changes depending
+	// on the pointer.
+
 	p1.toString() // Print format. Can get spesific formatted data.
 
-	p1Pointer := &p1
-	p1Pointer.updateName("Semihhh")
+	// p1Pointer := &p1
+	// p1Pointer.updateName("Semihhh")
+	// p1.toString()
+ 
+	// Bothersome way of doing pointer handling.
+	// Shortcut:
+
+	p1.updateName("James")
 	p1.toString()
+
+	// If you keep the method receiving a pointer but send the 
+	// object instead, go makes the pointer handling by itself.
+
+	// Basically, if you are going to update a local data which is
+	// embedded in the ram, use a pointer for reciever.
 }
 
-func (pointerToPerson *person) updateName(newName string){
+// *person is an object which is a pointer to a type
+// *pointerToPerson is an object which comes from the memory adress of pointerToPerson
+
+func (pointerToPerson *person)  updateName(newName string){
 	(*pointerToPerson).firstName = newName
 } 
 
